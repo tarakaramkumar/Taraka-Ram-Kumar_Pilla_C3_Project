56b6bd16da1381545a9186269611bd2d9cbcbf24
tarakaramkumar <50259242+tarakaramkumar@users.noreply.github.com>
2022-10-12 15:44:33 +0530

    Part3: Solution

---
+++ Restaurant_project+-Student+version/New Restp/src/main/java/restaurantNotFoundException.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class restaurantNotFoundException extends Throwable {
+    public restaurantNotFoundException(String restaurantName) {
+        super(restaurantName);
+    }
+}
