91c1940e57285084c88f7be8ac8108354f492b34
tarakaramkumar <50259242+tarakaramkumar@users.noreply.github.com>
2022-10-12 15:38:29 +0530

    Delete Restaurant_project+-Student+version/New Restp directory

--- Restaurant_project+-Student+version/New Restp/src/main/java/itemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-public class itemNotFoundException extends Throwable {
-    public itemNotFoundException(String itemName) {
-        super(itemName);
-    }
-}
--- Restaurant_project+-Student+version/New Restp/src/main/java/restaurantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-public class restaurantNotFoundException extends Throwable {
-    public restaurantNotFoundException(String restaurantName) {
-        super(restaurantName);
-    }
-}
